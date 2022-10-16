3f9f807598ffc63d539c826d950c006ee911d5b8
Peter G. Marczis <marczis@vikingcube.com>
2022-10-16 12:03:39 +0200

    Working on the BTNHandler

---
+++ Core/Inc/BTNHandler.h
@@ -0,0 +1,17 @@
+/*
+ * BTNHandler.h
+ *
+ *  Created on: 16 Oct 2022
+ *      Author: PeterMarczis
+ */
+
+#ifndef BTNHANDLER_H_
+#define BTNHANDLER_H_
+
+class BTNHandler {
+public:
+	BTNHandler();
+	virtual ~BTNHandler();
+};
+
+#endif /* BTNHANDLER_H_ */
---
+++ Core/Src/BTNHandler.cpp
@@ -0,0 +1,18 @@
+/*
+ * BTNHandler.cpp
+ *
+ *  Created on: 16 Oct 2022
+ *      Author: PeterMarczis
+ */
+
+#include "BTNHandler.h"
+
+BTNHandler::BTNHandler() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BTNHandler::~BTNHandler() {
+	// TODO Auto-generated destructor stub
+}
+
